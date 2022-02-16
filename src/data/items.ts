7d3c874c66ddd90e3699787184c06efe5707dfe2
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-02-16 19:09:48 -0300

    Delete

--- src/data/items.ts
+++
@@ -1,9 +0,0 @@
-import { Item } from "../types/Item";
-
-export const items: Item[] = [
-    { date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.12, expense:true },
-    { date: new Date(2022, 0, 15), category: 'food', title: 'Burger King', value: 28, expense:true },
-    { date: new Date(2022, 0, 16), category: 'rent', title: 'Aluguel Apt', value: 2300, expense:true },
-    { date: new Date(2022, 0, 18), category: 'salary', title: 'Salário ACME', value: 4500, expense:true },
-    { date: new Date(2022, 1, 18), category: 'salary', title: 'Salário Amazonet', value: 4500, expense:true },
-];
